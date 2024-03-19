00f5990f96ed36bd5959f9d1ebf5ae8f03f93a4c
JanelleVargasPatino436 <142556831+JanelleVargasPatino436@users.noreply.github.com>
2024-03-19 17:06:37 +0000

    Tue, Mar 19, 2024, 10:06 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,8 +1,13 @@
 rulesBtn = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
+closeBtn = document.getElementById('close-btn')
 
 
 
 rulesBtn.addEventListener('click', () => {
     rules.classList.add('show')
+})
+
+closeBtn.addEventListener('click', () => {
+    rules.classList.remove('show')
 })
