eb5478f61cf9df462a01c188e1bf92b566483229
JanelleVargasPatino436 <142556831+JanelleVargasPatino436@users.noreply.github.com>
2024-04-02 17:22:48 +0000

    Tue, Apr 2, 2024, 10:22 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -181,12 +181,12 @@ function moveBall() {
                 if (
                     ball.x - ball.size > brick.x && //left brick side
                     ball.x + ball.size < brick.x + brick.w && //right
-                    ball.y + ball.size ? brick.y && //top
+                    ball.y + ball.size > brick.y && //top
                     ball.y - ball.size < brick.y + brick.h //bottom
-            ) {
+                 ) {
             ball.dy = -1 * ball.dy
             brick.visible = false
-              }
+                  }
         }
       })
     })
